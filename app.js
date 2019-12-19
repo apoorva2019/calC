2b44868fa9ecbec3eed8ede6863549fe0900318a
Apoorva rastogi <apoorrva.rastogi@gmail.com>
2019-12-18 20:55:20 -0600

    working

--- app.js
+++ app.js
@@ -1,99 +1,40 @@
-const calculator = {
-  displayValue: '0',
-  firstOperand: null,
-  waitingForSecondOperand: false,
-  operator: null,
-};
-
-function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
-
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+let display = document.getElementById("display");
+display.innerHTML = 0;
+function pressBtn(btn) {
+  let pressed = btn.value;
+  let expr = display.innerHTML;
+  if (pressed === '=') {
+    display.innerHTML = eval(expr);
+  }
+  else if (pressed==="<"){
+    display.innerHTML = display.innerHTML.substring(0, expr.length-1);
+  }
+  else if (pressed === ".") {
+    if (display.innerHTML.indexOf('.') !=-1 && display.innerHTML.match(/\/|\*|\+|-/g) === null || display.innerHTML.substring(expr.length-1, expr.length) == '.') {
+      display.innerHTML = display.innerHTML;
+    }
+    else {
+      display.innerHTML = display.innerHTML + pressed;
+    }
+  }
+  else if (pressed.match(/\/|\*|\+|-/g)){
+    if (display.innerHTML.substring(expr.length-1, expr.length).match(/\/|\*|\+|-/g)){
+      display.innerHTML = display.innerHTML.replace(display.innerHTML.substring(expr.length-1, expr.length).match(/\/|\*|\+|-/g), pressed);
+    }
+    else {
+      display.innerHTML = display.innerHTML + pressed;
+    }
+  }
+  else if (pressed === 'C') {
+    display.innerHTML = '0';
+  }
+  else {
+    if (display.innerHTML === '0') {
+      display.innerHTML = pressed;
+    }
+    else {
+    display.innerHTML = display.innerHTML+btn.value;
+    }
   }
 
-  console.log(calculator);
 }
-
-function inputDecimal(dot) {
-  // If the `displayValue` does not contain a decimal point
-  if (!calculator.displayValue.includes(dot)) {
-    // Append the decimal point
-    calculator.displayValue += dot;
-  }
-}
-
-function handleOperator(nextOperator) {
-  const { firstOperand, displayValue, operator } = calculator
-  const inputValue = parseFloat(displayValue);
-
-  if (operator && calculator.waitingForSecondOperand)  {
-    calculator.operator = nextOperator;
-    console.log(calculator);
-    return;
-  }
-
-  if (firstOperand == null) {
-    calculator.firstOperand = inputValue;
-  } else if (operator) {
-    const currentValue = firstOperand || 0;
-    const result = performCalculation[operator](currentValue, inputValue);
-
-    calculator.displayValue = String(result);
-    calculator.firstOperand = result;
-  }
-
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
-  console.log(calculator);
-}
-
-const performCalculation = {
-  '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
-
-  '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
-
-  '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
-
-  '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
-
-  '=': (firstOperand, secondOperand) => secondOperand
-};
-
-function updateDisplay() {
-  const display = document.querySelector('.calculator-screen');
-  display.value = calculator.displayValue;
-}
-
-updateDisplay();
-
-const keys = document.querySelector('.calculator-keys');
-keys.addEventListener('click', (event) => {
-  const { target } = event;
-  if (!target.matches('button')) {
-    return;
-  }
-
-  if (target.classList.contains('operator')) {
-    handleOperator(target.value);
-		updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('decimal')) {
-    inputDecimal(target.value);
-		updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains('all-clear')) {
-    console.log('clear', target.value);
-    return;
-  }
-
-  inputDigit(target.value);
-  updateDisplay();
-});
